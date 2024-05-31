fae292b3b222f11ce4181c88f4fe7d4b8a044483
Ihmaid <gabriel-ihmaid@ufrrj.br>
2024-05-31 13:36:32 -0300

    Turned send_email into a function, day 22

--- send_email.py
+++ send_email.py
@@ -1,5 +1,6 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(message):
@@ -7,7 +8,7 @@ def send_email(message):
     port = 465
 
     username = "teste1111python@gmail.com"
-    password = "bwck hrci xerb ypik"
+    password = os.getenv("PASSWORD")  # Environmental variable
     receiver = "teste1111python@gmail.com"
     context = ssl.create_default_context()
 
